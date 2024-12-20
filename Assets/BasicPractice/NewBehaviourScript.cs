edf23b3a763a26822f8c5647a5869eabcddada0d
BinhTran <fam.out16@gmail.com>
2024-12-20 21:17:55 +0700

    21:17 20.12.2024

--- Assets/BasicPractice/NewBehaviourScript.cs
+++ Assets/BasicPractice/NewBehaviourScript.cs
@@ -3,33 +3,33 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     Rigidbody rb;
+
+    const float GRAVITY = 9.81f;
+    float expectHeight = 2f;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.velocity = new Vector3(0, 0, 5);
     }
 
-    bool jump;
-
+    bool jumped;
+    Vector3 currentVelocity;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !jump)
+        if (Input.GetKeyDown(KeyCode.Space) && !jumped)
         {
-            jump = true;
+            rb.velocity += new Vector3(0, Mathf.Sqrt(2f * GRAVITY * expectHeight), 0);
+            currentVelocity = rb.velocity;
+            jumped = true;
         }
-    }
-
-    const float GRAVITY = -9.81f;
-
-    private void FixedUpdate()
-    {
-        if (jump)
+        if (jumped)
         {
-
-            if (rb.velocity.y == 0)
-            {
-                jump = false;
-            }
+            currentVelocity.y -= Time.deltaTime * GRAVITY;
+            currentVelocity.y = Mathf.Max(-GRAVITY, currentVelocity.y);
+            rb.velocity = currentVelocity;
+            if (rb.velocity.y == -GRAVITY)
+                jumped = false;
         }
     }
 }
